30cb696e2be93bf81a0b36b3cf4097a10bb7b4e2
Roberto C. Rivera <rivera.roberto89@gmail.com>
2014-03-26 17:06:59 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrimenesApi::Application.config.secret_key_base = 'a63825869850fcb9964474b29f6af73be408bb2b5d6410d00b8e630906400575ecde857269b29d9cdf5c5f699ba27d3438becee9cb7ec4345d55c30496bdc5b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrimenesApi::Application.config.session_store :cookie_store, key: '_crimenes-api_session'
