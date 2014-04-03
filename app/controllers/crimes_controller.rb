46f383e83b059746d0abf0493dc8585b59c90203
Roberto C. Rivera <rivera.roberto89@gmail.com>
2014-04-02 21:58:58 -0400

    Responding to url requests within polygon, from_date and to_date

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,41 +1,20 @@
 class CrimesController < ApplicationController
 
   def index
-  	location = [-67.131790, 18.001448]
+  	begin  
+	    polygon = JSON.parse(params[:polygon])
+	  	from_date = DateTime.strptime(params[:from_date], '%Y-%m-%d')
+	  	to_date = DateTime.strptime(params[:to_date], '%Y-%m-%d')
 
-  	sju = [-66.0583415, 18.3848264]
-  	gua = [-66.1122969, 18.359121]
-  	car = [-67.9809077, 18.4054882]
-
-
-
-
-  	#@crimes = Crime.where("property.time" => {:$gte => Time.utc(2013, 4, 1), :$lte => Time.now.utc})
-
-  	#@crimes = Crime.where("geometry.coordinates" => {"$within" => {"$box" => [sju, gua, car]}})
-
-
-  	#box = [sju, gua]
-
-  	@crimes = Crime.where({"geometry.coordinates" => {"$within" => {"$polygon" => [sju, gua, car]}}})
-
-    respond_to do |format|
+	  	@crimes = Crime.where({"geometry.coordinates" => {"$within" => {"$polygon" => polygon}}}).and("property.time" => {:$gte => from_date, :$lte => Time.now.utc}) 
+	  rescue  
+	    @crimes = nil
+	  end  
+	  respond_to do |format|
       format.json { render :json => @crimes }
     end
   end
-
 end
 
-
-
-
-
-#polygon
-#ul -67.131790, 18.476211
-#dl -67.131790, 18.001448
-#ur -65.640396, 18.476211
-#dr -65.640396, 18.001448
-
-#  	@crimes = Crime.where("property.time" => {:$gte => Time.utc(2013, 2, 1), :$lte => Time.now.utc}).geo_near(location).max_distance(1000).spherical
-
-#@crimes = Crime.where("property.time" => {:$gte => Time.utc(2013, 2, 1), :$lte => Time.now.utc}).geo_near(location).max_distance(1000).spherical.max_distance.distance_multiplier(29)
+# Sample url request
+# http://localhost:3000/crimes?polygon=[[-67.9809077,%2018.4054882],[-66.1122969,%2018.359121],[-66.0583415,%2018.3848264]]&from_date=2013-04-01&to_date=2014-04-22
